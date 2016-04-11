530885d2f355ab48ec539dfea3b9c178b734a703
hamawaki <oz00ms03@gmail.com>
2016-04-11 16:28:46 +0900

    初期コミット

---
+++ AzureWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureWebApplication.MvcApplication" Language="C#" %>
---
+++ AzureWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureWebApplication.Startup))]
+namespace AzureWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
